3ba97a28136c17a0ce439e082fd33fb9c8f5b2ea
Arkadi Chubrik <arkadi@chubrik.org>
2023-12-11 03:22:39 +0400

    Computer emulator

---
+++ ComputerEmulator/Program.cs
@@ -0,0 +1,9 @@
+﻿using ComputerEmulator;
+
+Console.Utils.MaximizeWindow();
+var ram = new Ram();
+Console.Pin(ram.Display);
+var game = new Game();
+ram.Load(game.Bytes());
+var cpu = new Cpu(ram);
+cpu.Run();
