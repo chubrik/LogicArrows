6a8a04c36a9cd41e6b10a21bc449098d08e59019
Arkadi Chubrik <arkadi@chubrik.org>
2024-11-10 16:47:04 +0400

    Game of Life

--- ComputerEmulator/Program.cs
+++ ComputerEmulator/Program.cs
@@ -1,19 +1,19 @@
 ﻿Console.Extras.WindowMaximize();
 
 // V1 - Space Fight!
-{
-    var ram = new ComputerEmulator.V1.Ram();
-    Console.Pin(ram.Display);
-    ram.Load(ComputerEmulator.V1.SpaceFight.Bytes());
-    var cpu = new ComputerEmulator.V1.Cpu(ram);
-    cpu.Run();
-}
-
-// V2 - Game
 //{
-//    var ram = new ComputerEmulator.V2.Ram();
+//    var ram = new ComputerEmulator.V1.Ram();
 //    Console.Pin(ram.Display);
-//    ram.Load(ComputerEmulator.V2.Game.Bytes());
-//    var cpu = new ComputerEmulator.V2.Cpu(ram);
+//    ram.Load(ComputerEmulator.V1.SpaceFight.Bytes());
+//    var cpu = new ComputerEmulator.V1.Cpu(ram);
 //    cpu.Run();
 //}
+
+// V2 - Game of Life
+{
+    var ram = new ComputerEmulator.V2.Ram();
+    Console.Pin(ram.Display);
+    ram.Load(ComputerEmulator.V2.Game.Bytes());
+    var cpu = new ComputerEmulator.V2.Cpu(ram);
+    cpu.Run();
+}
